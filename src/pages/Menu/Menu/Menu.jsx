67ac0261fc2bf8345b735b563c27b30ff3d80867
abubakkar-js-dev <mdabubakkars182@gmail.com>
2024-12-31 09:50:11 +0600

    initial commit

---
+++ src/pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet-async";
+
+const Menu = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Home | Bistro Boss Rastaurant</title>
+      </Helmet>
+      Menu
+    </div>
+  );
+};
+
+export default Menu;
