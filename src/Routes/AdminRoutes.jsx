8eda624abcce9957f0e8ce47e53d28223cf47dc4
abubakkar-js-dev <mdabubakkars182@gmail.com>
2025-01-08 08:35:07 +0600

    updated code

--- src/Routes/AdminRoutes.jsx
+++ src/Routes/AdminRoutes.jsx
@@ -1,19 +1,20 @@
-import { Navigate, useLocation } from "react-router-dom";
-import Loading from "../components/Loading/Loading";
-import useAdmin from "../hooks/useAdmin";
-import useAuth from "../hooks/useAuth";
+import useAuth from '../hooks/useAuth';
+import useAdmin from '../hooks/useAdmin';
+import { Navigate, useLocation } from 'react-router-dom';
+import Loading from '../components/Loading/Loading';
 
 const AdminRoutes = ({children}) => {
-    const {user,loading}=useAuth();
-    const {isAdmin,isLoading}=useAdmin();
-    const location = useLocation();
+   const {loading,user} = useAuth();
+   const {isAdmin,isLoading} = useAdmin();
+   const locaiton = useLocation();
    if(loading || isLoading){
     return <Loading/>
    }
    if(user && isAdmin){
     return children;
-   }    
-   return <Navigate to="/login" state={{from:location.pathname}} replace />;  
+   }
+   
+   return <Navigate to="/login" state={{from:locaiton.pathname}} replace />
 };
 
 export default AdminRoutes;
