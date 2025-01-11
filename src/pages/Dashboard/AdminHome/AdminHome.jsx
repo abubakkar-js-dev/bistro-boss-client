407ebaa41c9c701b74e7359d60b10a5d7e8b0a44
abubakkar-js-dev <mdabubakkars182@gmail.com>
2025-01-11 14:04:01 +0600

    add: chart in admin home

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -5,19 +5,72 @@ import { FaTruck, FaUsers } from "react-icons/fa";
 import { FaMoneyBill } from "react-icons/fa6";
 import { MdOutlineProductionQuantityLimits } from "react-icons/md";
 import Loading from "../../../components/Loading/Loading";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { PieChart, Pie, Sector, ResponsiveContainer } from 'recharts';
+
+
+// color for grap chart
 
 const AdminHome = () => {
   const axiosSecure = useAxiosSecure();
   const { user } = useAuth();
   const { data: stats = {},isPending:statsLoading } = useQuery({
     queryKey: ["admin-stats"],
+    enabled: !!user?.email,
     queryFn: async () => {
       const res = await axiosSecure.get("/admin-stats");
       return res.data;
     },
   });
 
-  const { menuItems, orders, payments, revenue, users } = stats;
+  const {data: chartData=[]}= useQuery({
+    queryKey: ['order-stats'],
+    queryFn: async()=>{
+        const res = await axiosSecure.get('/order-stats');
+        return res.data;
+    }
+  })
+
+  console.log(chartData);
+
+  const {  orders, revenue, users } = stats;
+
+  // for grap chart
+
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+    Z`;
+  };
+
+  const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+  
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
+
+
+
+  //  for pie chart
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+  
 
 
   if(statsLoading){
@@ -62,6 +115,54 @@ const AdminHome = () => {
           <div className="stat-title">Orders</div>
         </div>
       </div>
+
+    {/* charts */}
+    <div className="flex flex-col xl:flex-row">
+      {/* bar chart */}
+      <div className="xl:w-1/2">
+      <BarChart
+      width={500}
+      height={300}
+      data={chartData}
+      margin={{
+        top: 20,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="project" />
+      <YAxis />
+      <Bar dataKey="revenue" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+        {chartData.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+        ))}
+      </Bar>
+    </BarChart>
+      </div>
+      {/* pie chart */}
+      <div className="xl:w-1/2">
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart width={400} height={400}>
+          <Pie
+            data={chartData}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="quantity"
+          >
+            {chartData.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+      </div>
+    </div>
     </div>
   );
 };
